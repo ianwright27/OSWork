12824def7e9ae1492739b08f2076bad54faedf09
cyberhawk <cyberhawk@localhost.localdomain>
2020-11-23 21:50:07 +0300

    final

--- a1q1.c
+++ a1q1.c
@@ -5,26 +5,26 @@
 int main(int argc, char* argv[]){
 	// will hold PIDs for each level
 	int grandParentPID, parentPID, childPID;
-	
+
 	// get process id of our main process (grandparent)
 	grandParentPID = getpid();
-	
+
 	// create child process (parent)
 	int parentpid = fork();
-	
+
 	if (parentpid == 0){
 		// get pid of parent
 		parentPID = getpid();
-		
+
 		// create new child of parent
 		int grandchildpid = fork();
-		
+
 		if (grandchildpid == 0){
 			childPID = getpid();
 			// output
 			printf("My process id is %d, my parent's id is %d, and my Grandparent's id is %d\n", childPID, parentPID,  grandParentPID);
 		}
 	}
-	
+
 	return 0;
 }
