5c3ca1b7a6ce1c2a8614173f07112093085a2aa5
cyberhawk <cyberhawk@localhost.localdomain>
2020-11-24 22:07:35 +0300

    a1q3.c

--- a1q3.c
+++ a1q3.c
@@ -1,26 +1,7 @@
-# include <stdio.h>
-# include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-void *sum_of_integers(void *arguments){
-    struct sum_args *args = (struct sum_args *)arguments;
-    int n = args->n;
-    int m = args->m;
-
-    // sum integers between n and m
-    for(int i = n; i <= m; ++i){
-        pthread_mutex_lock(&mVar);
-        total = total + i;
-        pthread_mutex_unlock(&mVar);
-    }
-
-    // now we access total once more
-    pthread_mutex_lock(&mVar);
-    printf("[+] Total is :%d", total);
-    pthread_mutex_unlock(&mVar);
-
-    //exit
-    pthread_exit(NULL);
-}
 
 // all arguments for sum function
 struct sum_args {
@@ -32,45 +13,94 @@ struct sum_args {
 // global for total sum
 int total=0;
 
+
 // To prevent segmentation fault since each thread might be trying to access Total
-pthread_mutex_t mVar=PTHREAD_MUTEX_INITIALIZER;
+pthread_mutex_t MUT=PTHREAD_MUTEX_INITIALIZER;
+
+
+// function to sum integers
+void *sum_of_integers(void *arguments){
+    struct sum_args *args = (struct sum_args *)arguments;
+    int n = args->n;
+    int m = args->m;
+
+    // sum integers between n and m
+    for(int i = n; i <= m; ++i){
+        pthread_mutex_lock(&MUT);
+        total = total + i;
+        pthread_mutex_unlock(&MUT);
+    }
+    
+    // now we access total once more
+    pthread_mutex_lock(&MUT);
+    printf("[+] Total is :%d", total);
+    pthread_mutex_unlock(&MUT);
+    
+    //exit
+    pthread_exit(NULL);
+}
+
 
 int main(int argc, char* argv[]){
     int n, m;
-
+	
+	// to capture the integers
+	long int_n, int_m, int_x;
+	int_n = strtol(argv[0], NULL, 10);
+	int_m = strtol(argv[1], NULL, 10);
+	int_x = strtol(argv[2], NULL, 10);
+	
     // number of threads to create
     int numThreads;
 
     // grab arguments (typecast to int)
-    n = (int)argv[0];
-    m = (int)argv[1];
-    numThreads = (int)argv[2];
+    n = int_n;
+    m = int_m;
+    numThreads = int_x;
 
+
+	// Debugging
+	printf("\nassignments to int versions of sys args");
+	
     // assign arguments to struct
-    struct sys_args *args = (struct sys_args *)args;
-    args -> n = n;
-    args -> n = m;
+    struct sum_args *args;
+    args->n = n;
+    args->n = m;
+
+
+	// Debugging
+	printf("\nsum_args struct assigned");
 
     // create pthread array of "numThread" elements
     pthread_t thread[numThreads];
-
+	
+	/*	
+	// Debugging
+	printf("\narray of threads created");
+	*/
+    
     for (int i=0; i<numThreads; ++i){
 
        int thread_status = pthread_create(&thread[i], NULL, &sum_of_integers, (void*)&args);
 
        // was pthread_create successful?
-       if (thread_status !=0)
-       {
-            printf("Error creating thread %d", i);
+       if (thread_status !=0){
+		   
+            printf("\nError creating thread %d", i);
+
        }
+       
     }
 
     //Join all threads
     for(int i = 0; i < numThreads; ++i){
-     pthread_join(thread[i], NULL);
-    }
 
-    cout << "All threads completed." ;
+     pthread_join(thread[i], NULL);
+
+    }
+    
+
+    printf("All threads completed.");
 
     return 0;
 }
